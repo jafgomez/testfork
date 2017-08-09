5417f49049dfac5ec17496b1711f41469ff3ae08
jafgomez <juanantonio.fernandez@bitpangea.org>
2017-08-09 13:13:56 +0200

    test

--- Assets/ForkController.cs
+++ Assets/ForkController.cs
@@ -9,11 +9,14 @@ public class ForkController : MonoBehaviour
 
     public float MaxForkLocalHeight = 5f;
 
-    [Range(0, 40)]
+    [Range(-40, 40)]
     public float enginePower;
 
-    [Range(-15, 15)]
-    public int ForkRotation;
+    [Range(-15f, 15f)]
+    public float ForkRotation;
+
+    [Range(-0.2f, 0.2f)]
+    public float ForkDisplacement;
 
     // Use this for initialization
     void Start () {
@@ -23,14 +26,18 @@ public class ForkController : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-
-	    if (fork.transform.localPosition.y >= MaxForkLocalHeight)
+        if (fork.transform.localPosition.y >= MaxForkLocalHeight)
 	        enginePower = 0f;
 
-        fork.AddForce(fork.transform.up * enginePower, ForceMode.Force);
+	    fork.AddForce(fork.transform.up * enginePower, ForceMode.Force);
 
 	    fork.transform.localRotation = Quaternion.Slerp(fork.transform.localRotation,
 	        Quaternion.AngleAxis(ForkRotation, Vector3.right), 10*Time.deltaTime);
+
+	    Vector3 l = fork.transform.localPosition;
+	    l.x = Mathf.Lerp(fork.transform.localPosition.x, ForkDisplacement, 10*Time.deltaTime);
+	    fork.transform.localPosition = l;
+	    
 
 	}
 }
