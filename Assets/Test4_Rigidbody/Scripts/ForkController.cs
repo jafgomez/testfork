bdde365324c5086e745d3cc091e6ada0a5e28931
Juan Antonio Fernandez <juanantonio.fernandez@bitpangea.org>
2017-08-17 13:38:33 +0200

    test with rigid body
    To much complicated

---
+++ Assets/Test4_Rigidbody/Scripts/ForkController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForkController : MonoBehaviour
+{
+    public float maxForce = 20f;
+    public ForkLiftInput LiftInput;
+    public Transform TopBlocker;
+    public Transform BottomBlocker;
+    public Transform CenterOfMass;
+
+    [SerializeField]
+    private float _upForce;
+    private Vector3 _topBlockerOffset;
+    private Vector3 _bottomBlockerOffset;
+    private Rigidbody _myRigidBody;
+    [SerializeField]
+    private float _rawVerticalInput;
+
+
+    private void Awake()
+    {
+        _topBlockerOffset = transform.position - TopBlocker.position;
+        _bottomBlockerOffset = transform.position - BottomBlocker.position;
+        _myRigidBody = GetComponent<Rigidbody>();
+        _myRigidBody.centerOfMass = CenterOfMass.localPosition;
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    _rawVerticalInput = LiftInput.forkVerticalInput * .25f;
+	    float rawHorizontalInput = LiftInput.forkHorizontalInput;
+
+	    if (_rawVerticalInput != 0.0f)
+	    {
+            if (_rawVerticalInput < 0f)
+            {
+                _upForce = -1f;
+            }
+            else
+            {
+                if (_upForce <= maxForce)
+                    _upForce = maxForce;
+            }
+
+            TopBlocker.position = transform.position + _topBlockerOffset;
+            BottomBlocker.position = transform.position - _bottomBlockerOffset;
+
+
+	    }
+	    else
+	    {
+	        _upForce = 0f;
+	    }
+
+
+
+
+
+
+	}
+
+    private void FixedUpdate()
+    {
+        if (_upForce != 0f)
+            _myRigidBody.AddForce(Vector3.up * _upForce, ForceMode.Force);
+    }
+
+
+}
---
+++ Assets/Test4_Rigidbody/Scripts/ForkLiftInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForkLiftInput : MonoBehaviour
+{
+
+    public float forkVerticalInput;
+    public float forkHorizontalInput;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    string[] joy = Input.GetJoystickNames();
+	    for (int i = 0; i < joy.Length; i++)
+	    {
+	        Debug.Log(joy[i]);
+	    }
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    forkVerticalInput = Input.GetAxisRaw("Vertical");
+        forkHorizontalInput = Input.GetAxisRaw("Horizontal");
+         
+	}
+}
