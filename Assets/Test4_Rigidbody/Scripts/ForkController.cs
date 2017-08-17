38fb70c9dea07abc62a3b8bd72587aa1ebaa6d36
Juan Antonio Fernandez <juanantonio.fernandez@bitpangea.org>
2017-08-17 16:46:07 +0200

    WIP (better controller)

--- Assets/Test4_Rigidbody/Scripts/ForkController.cs
+++ Assets/Test4_Rigidbody/Scripts/ForkController.cs
@@ -5,76 +5,46 @@ using UnityEngine;
 
 public class ForkController : MonoBehaviour
 {
-    public float maxForce = 20f;
     public ForkLiftInput LiftInput;
-    public Transform TopBlocker;
-    public Transform BottomBlocker;
-    public Transform CenterOfMass;
-
-    [SerializeField]
-    private float _upForce;
-    private Vector3 _topBlockerOffset;
-    private Vector3 _bottomBlockerOffset;
-    private Rigidbody _myRigidBody;
-    [SerializeField]
-    private float _rawVerticalInput;
-
-
+    public ForkDescriptor ForkDescriptor;
+    public Transform LogicFork;
+    public float SpeedTranslateFactor; //Platform travel speed
+    public Vector3 MaxTranslation; //The maximum translation limit of the platform
+    public Vector3 MinTranslation; //The minimum translation limit of the platform
+    public float MaxYmast; //The maximum height of the mast
+    public float MinYmast; //The minimum height of the mast
+    private Rigidbody _logicForkRigidbody;
+    
     private void Awake()
     {
-        _topBlockerOffset = transform.position - TopBlocker.position;
-        _bottomBlockerOffset = transform.position - BottomBlocker.position;
-        _myRigidBody = GetComponent<Rigidbody>();
-        _myRigidBody.centerOfMass = CenterOfMass.localPosition;
+        _logicForkRigidbody = LogicFork.GetComponent<Rigidbody>();
     }
-
-
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-
-	    _rawVerticalInput = LiftInput.forkVerticalInput * .25f;
-	    float rawHorizontalInput = LiftInput.forkHorizontalInput;
-
-	    if (_rawVerticalInput != 0.0f)
-	    {
-            if (_rawVerticalInput < 0f)
-            {
-                _upForce = -1f;
-            }
-            else
-            {
-                if (_upForce <= maxForce)
-                    _upForce = maxForce;
-            }
-
-            TopBlocker.position = transform.position + _topBlockerOffset;
-            BottomBlocker.position = transform.position - _bottomBlockerOffset;
-
-
-	    }
-	    else
-	    {
-	        _upForce = 0f;
-	    }
-
-
-
-
-
-
-	}
+    
 
     private void FixedUpdate()
     {
-        if (_upForce != 0f)
-            _myRigidBody.AddForce(Vector3.up * _upForce, ForceMode.Force);
+
+        float rawVerticalInput = LiftInput.forkVerticalInput;
+        float rawHorizontalInput = LiftInput.forkHorizontalInput;
+
+        
+        float factorV = SpeedTranslateFactor*rawVerticalInput*Time.deltaTime;
+        float factorH = SpeedTranslateFactor*rawHorizontalInput*Time.deltaTime;
+
+        Vector3 translation = new Vector3(0f, factorV, factorH);
+        
+        Vector3 logicPosition = translation + _logicForkRigidbody.position;
+
+        _logicForkRigidbody.MovePosition(logicPosition);
+        ForkDescriptor.Fork.Translate(translation);
+        
+        //if (mastMoveTrue)
+        //{
+        //    mast.Translate(Vector3.up * speedTranslate * Time.deltaTime);
+        //}
+
+
+
     }
 
 
