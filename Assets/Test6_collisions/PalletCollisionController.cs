72881f498d77b14a4ed16f853bcd855c10b325cb
jafgomez <juanantonio.fernandez@bitpangea.org>
2017-09-29 14:59:59 +0200

    more test

---
+++ Assets/Test6_collisions/PalletCollisionController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalletCollisionController : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+
+            Debug.Log(contact.separation);
+
+        }
+
+    }
+}
